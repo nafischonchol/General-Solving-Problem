6334529df3d9c935e57801c3094bb7e0d313100d
nafischonchol <nafisislam161@gmail.com>
2020-02-17 03:12:27 +0600

    Hamming Distance
    Hamming Distance between two integers is the number of bits which are different at same position in both numbers

---
+++ Hamming Distance.cpp
@@ -0,0 +1,11 @@
+int hammingDistance(int x, int y) {
+        int a=x^y;
+        int c=0;
+        while(a!=0)
+        {
+            if(a%2!=0)
+                c++;
+            a=a/2;
+        }
+        return c;
+    }
